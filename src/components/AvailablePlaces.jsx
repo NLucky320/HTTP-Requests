879ec314cada8ba6c4d499c55dbd3f0ae7d9b2d8
lucky <luckyes144@gmail.com>
2026-08-24 23:09:46 +0600

    Transforming Fetched Data

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 import ErrorPage from './Error.jsx';
+import { sortPlacesByDistance } from '../loc.js';
 export default function AvailablePlaces({ onSelectPlace }) {
 
   const [isFetching, setIsFetching] = useState(false)
@@ -15,21 +16,25 @@ export default function AvailablePlaces({ onSelectPlace }) {
         const response = await fetch('http://localhost:3000/places');
         const resData = await response.json();
         if (!response.ok) {
-
           throw new Error('Failed to fetch places');
         }
-        setAvailablePlaces(resData.places);
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(resData.places, position.coords.latitude, position.coords.longitude)
+          setAvailablePlaces(sortedPlaces);
+            setIsFetching(false);
+        });
       } catch (error) {
         setError(error)
+          setIsFetching(false);
       }
-      setIsFetching(false);
+    
     }
     fetchPlaces();
   }, [])
 
-if (error) {    
-  return <ErrorPage title="An error occurred!" message={error.message} />;
-}
+  if (error) {
+    return <ErrorPage title="An error occurred!" message={error.message} />;
+  }
   return (
     <Places
       title="Available Places"
